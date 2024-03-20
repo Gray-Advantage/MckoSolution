e376e13c6a882294b6b469a25ac341971f8c8e6d
Gray Advantage <sergeyindra2006@gmail.com>
2024-03-20 16:52:09 +0300

    feat: add solution_5.py, but without hash

---
+++ solution_5.py
@@ -0,0 +1,27 @@
+IN_FILE_NAME = "scientist.txt"  # Имя входного файла
+OUT_FILE_NAME = "scientist_with_hash.csv"  # Имя выходного файла
+
+
+def create_hash(string):
+    """Функция, которая генерирует хэш
+
+    :return: str
+    """
+
+    return hash(string)
+
+
+with open(IN_FILE_NAME, encoding="utf8") as in_file:
+    title, *lines = [elem.rstrip().split("#") for elem in in_file.readlines()]
+
+
+# Добавляем новые столбцы
+title = ["hash"] + title
+for index in range(len(lines)):
+    lines[index] = [create_hash(lines[index][0])] + lines[index]
+
+
+with open(OUT_FILE_NAME, "w", encoding="utf8", newline="") as out_file:
+    print(*title, sep="#", file=out_file)
+    for row in lines:
+        print(*row, sep="#", file=out_file)
